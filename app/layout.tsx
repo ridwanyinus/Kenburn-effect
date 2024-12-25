9075fd036be09705cc0b23bfe7c356296c4da39e
Ridwan <ridwanyinusa15@gmail.com>
2024-12-25 22:09:07 +0100

    chore: remove font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import Script from 'next/script';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'kenburns effect',
@@ -25,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`font-sans antialiased`}>
         {children}
         <Script src='./js/jquery-3.4.1.min.js' />
         <Script src='./vendors/revolution/js/jquery.themepunch.tools.min.js' strategy='afterInteractive' />
