29cf36338dc70b1ae7e6cde0fbd95dc57394d55a
Ridwan <ridwanyinusa15@gmail.com>
2024-12-25 22:33:54 +0100

    chore:(meta) update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'kenburns effect',
+  title: 'React nextjs kenburns effect',
   description: 'Made with 🖤',
 };
 
